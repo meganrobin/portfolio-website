c02bc127a9734d42c6947d3ca1f7a3e119ef814b
meganrobin <156228985+meganrobin@users.noreply.github.com>
2025-12-09 11:22:37 -0800

    Clean up App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Profile from './components/Profile/Profile'
 import Projects from './components/Projects/Projects'
-import Work from './components/Work/Work'
+// import Work from './components/Work/Work'
 
 import Footer from './components/Footer/Footer'
 
